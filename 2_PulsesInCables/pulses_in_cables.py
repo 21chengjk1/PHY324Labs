2672d17eb96b1ba9eb7ad49c93917664cb28372b
Jack <chengkinhangjack@gmail.com>
2025-11-09 12:17:21 -0500

    upload_data, initialize file

---
+++ 2_PulsesInCables/pulses_in_cables.py
@@ -0,0 +1,13 @@
+"""
+Python file to do pulses in Cables Experiment.
+"""
+
+def main():
+    print("---START---")
+    print("Hello Welcome to the Pulses in Cables experiment!")
+
+    print("---END---")
+
+
+if __name__ == '__main__':
+    main()
