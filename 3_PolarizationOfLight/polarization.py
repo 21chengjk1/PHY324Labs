87a41c2afbf99691968e158b244cb18721381634
Jack <chengkinhangjack@gmail.com>
2025-11-25 00:28:46 -0500

    initializing files and data for free choice 3.

---
+++ 3_PolarizationOfLight/polarization.py
@@ -0,0 +1,18 @@
+"""
+Python file to do Polarization in Light Experiment.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    print("=== START ===")
+    print("Hello! Welcome to the Polarization in Light Experiment!")
+
+    # CODE...
+
+    print("=== END ===")
+
+
+if __name__ == "__main__":
+    main()
